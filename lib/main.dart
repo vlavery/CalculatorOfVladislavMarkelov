581ac9e849159364445be238cebb5cc809ce3566
Vladislav <136612743+vlavery@users.noreply.github.com>
2024-04-14 19:42:13 +0300

    Added firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // Импортируем пакет Firebase Core
 import 'calculator_screen.dart';
 import 'converter_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // Инициализируем Firebase
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key); // Исправляем определение конструктора
 
   @override
   Widget build(BuildContext context) {
